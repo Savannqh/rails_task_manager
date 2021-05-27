ee1506efb19b1082f25b819e305d23dc6b5d9faf
code ~/.zshrc <zarourisamir@hotmail.com>
2021-05-27 11:18:07 +0200

    taskbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks/:id' , to: 'tasks#index'
+  get 'tasks/new' , to: 'tasks#new' 
+  
+  get 'tasks' => 'tasks#index'
+
+  get 'tasks/:id' , to:'tasks#show' , as: :task
+
+  post 'tasks' , to: 'tasks#create' 
+ 
 end
