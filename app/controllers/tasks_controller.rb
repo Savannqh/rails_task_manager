d80b8b641def5dabed24f3c5239b914f96c13b00
code ~/.zshrc <zarourisamir@hotmail.com>
2021-05-28 12:56:14 +0200

    delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,9 +22,17 @@ def edit
        @task.update(task_params)
        redirect_to task_path(@task)
     end
+def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+
+    end
     
     private
     def task_params
         params.require(:task).permit(:title , :details , :completed)
     end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
   get 'tasks/:id/edit', to:'tasks#edit'
 
   patch 'tasks/:id' , to:'tasks#update'
-end
+
+  delete 'tasks/:id' , to: 'tasks#destroy'
+
+end 
